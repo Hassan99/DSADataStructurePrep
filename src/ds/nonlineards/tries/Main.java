3863c57bbf005fc79dd1babe04e33c0ca513eebf
NoorulHassan <hassan.m@invenzolabs.com>
2026-09-18 21:29:41 +0530

    trie - pre and post order traversal

--- src/ds/nonlineards/tries/Main.java
+++ src/ds/nonlineards/tries/Main.java
@@ -3,9 +3,9 @@ package ds.nonlineards.tries;
 public class Main {
     public static void main(String[] args) {
         Trie trie= new Trie();
-        trie.insertTrie("categoryty");
-        trie.insertTrie("can");
+        trie.insertTrie("category");
         System.out.println(trie.search("category"));
+        trie.traversePost();
         System.out.println("Completed");
     }
 }
--- src/ds/nonlineards/tries/Trie.java
+++ src/ds/nonlineards/tries/Trie.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 public class Trie {
 
 
-    class Node{
+    class Node {
         private char value;
-        private HashMap<Character,Node> node = new HashMap<>();
+        private HashMap<Character, Node> node = new HashMap<>();
         private boolean isEndOfNode = false;
 
         public Node(char value) {
@@ -30,40 +30,70 @@ public class Trie {
         public void setEndOfNode(boolean endOfNode) {
             isEndOfNode = endOfNode;
         }
-        public boolean hasChild(char ch){
+
+        public boolean hasChild(char ch) {
             return node.containsKey(ch);
         }
-        public void addChild(char ch,Node node){
-            this.node.put(ch,node);
+
+        public void addChild(char ch, Node node) {
+            this.node.put(ch, node);
         }
-        public Node getChild(char ch){
+
+        public Node getChild(char ch) {
             return this.node.get(ch);
         }
+
+        public Node[] getAllChildren() {
+            return  node.values().toArray(new Node[0]);
+        }
     }
+
     private Node rootNode = new Node(' ');
 
-    public void addCharacter(char ch,Node node ){
+    public void traversePre() {
+        preOrderTraversal(rootNode);
+    }
+    public void traversePost() {
+        postOrderTraversal(rootNode);
+    }
+
+    private void preOrderTraversal(Node node) {
+        System.out.println(node.value);
+        Node[] allChild =  node.getAllChildren();
+        for (Node child : allChild) {
+            preOrderTraversal(child);
+        }
+    }
+    private void postOrderTraversal(Node node) {
+        Node[] allChild =  node.getAllChildren();
+        for (Node child : allChild) {
+            postOrderTraversal(child);
+        }
+        System.out.println(node.value);
+
 
     }
 
-    public void insertTrie(String word){
+
+    public void insertTrie(String word) {
         Node current = rootNode;
-        for(char ch:word.toCharArray()){
-            if(!current.hasChild(ch)){
-                current.addChild(ch,new Node(ch));
+        for (char ch : word.toCharArray()) {
+            if (!current.hasChild(ch)) {
+                current.addChild(ch, new Node(ch));
             }
             current = current.getChild(ch);
         }
-        current.isEndOfNode =true;
+        current.isEndOfNode = true;
     }
-    public boolean search(String word){
+
+    public boolean search(String word) {
         Node current = rootNode;
-        for(char ch:word.toCharArray()){
-            if(current.hasChild(ch)){
+        for (char ch : word.toCharArray()) {
+            if (current.hasChild(ch)) {
                 current = current.getChild(ch);
             }
 
         }
-       return  current.isEndOfNode();
+        return current.isEndOfNode();
     }
 }
