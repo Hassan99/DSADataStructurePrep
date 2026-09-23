eb602b198a51bf44ff6ee88d391ba895cf83c448
NoorulHassan <hassan.m@invenzolabs.com>
2026-09-23 20:26:18 +0530

    Removal of the word

--- src/ds/nonlineards/tries/Main.java
+++ src/ds/nonlineards/tries/Main.java
@@ -2,10 +2,17 @@ package ds.nonlineards.tries;
 
 public class Main {
     public static void main(String[] args) {
-        Trie trie= new Trie();
+        Trie trie = new Trie();
         trie.insertTrie("category");
+        trie.insertTrie("cat");
+        trie.insertTrie("catering");
+        System.out.println();
+        System.out.println();
+        trie.removeWord("catr");
+
         System.out.println(trie.search("category"));
-        trie.traversePost();
-        System.out.println("Completed");
+        System.out.println(trie.search("cat"));
+        System.out.println(trie.search("catering"));
+
     }
 }
--- src/ds/nonlineards/tries/Trie.java
+++ src/ds/nonlineards/tries/Trie.java
@@ -44,7 +44,11 @@ public class Trie {
         }
 
         public Node[] getAllChildren() {
-            return  node.values().toArray(new Node[0]);
+            return node.values().toArray(new Node[0]);
+        }
+
+        public void remove(char ch) {
+            node.remove(ch);
         }
     }
 
@@ -53,19 +57,21 @@ public class Trie {
     public void traversePre() {
         preOrderTraversal(rootNode);
     }
+
     public void traversePost() {
         postOrderTraversal(rootNode);
     }
 
     private void preOrderTraversal(Node node) {
         System.out.println(node.value);
-        Node[] allChild =  node.getAllChildren();
+        Node[] allChild = node.getAllChildren();
         for (Node child : allChild) {
             preOrderTraversal(child);
         }
     }
+
     private void postOrderTraversal(Node node) {
-        Node[] allChild =  node.getAllChildren();
+        Node[] allChild = node.getAllChildren();
         for (Node child : allChild) {
             postOrderTraversal(child);
         }
@@ -96,4 +102,50 @@ public class Trie {
         }
         return current.isEndOfNode();
     }
+
+    public void removeWord(String word) {
+        removeWord(rootNode, word, 0);
+    }
+
+    private void removeWord(Node node, String word, int index) {
+        if(index == word.length()){
+            node.isEndOfNode = false;
+            return;
+        }
+
+        char ch = word.charAt(index);
+        Node child = node.getChild(ch);
+        if(child==null){
+            return;
+        }
+        removeWord(child, word, index + 1);
+        if(child.getAllChildren().length==0 && !child.isEndOfNode){
+            child.remove(ch);
+        }
+    }
+
+
+
+   /* private void removeWord(Node node, String word) {
+        Node current = node;
+        for (char ch : word.toCharArray()) {
+            if (current.hasChild(ch)) {
+                current = current.getChild(ch);
+            }else{
+                System.out.println("Word not exist..");
+                return;
+            }
+        }
+        if (current.isEndOfNode()) {
+            current.setEndOfNode(false);
+            if(current.getAllChildren().length==0){
+
+            }
+        }else{
+            System.out.println("This word not found..");
+        }
+
+
+    }
+*/
 }
